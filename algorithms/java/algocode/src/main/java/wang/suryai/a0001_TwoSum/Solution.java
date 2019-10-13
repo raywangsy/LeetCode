5bfcea9047462ed7ae7dca0eaa521dd009c47ac5
suryaiwang <suryaiwang@gmail.com>
2019-10-13 15:56:28 +0800

    update README.md

--- algorithms/java/algocode/src/main/java/wang/suryai/a0001_TwoSum/Solution.java
+++ algorithms/java/algocode/src/main/java/wang/suryai/a0001_TwoSum/Solution.java
@@ -36,7 +36,11 @@ public class Solution {
      * @param args
      */
     public static void main(String[] args) {
-
+        int nums[] = new int[]{2, 7, 11, 15};
+        int target = 9;
+        Solution solution = new Solution();
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("[" + result[0] +", " + result[1] + "]");
     }
 
     /**
---
+++ algorithms/java/algocode/src/main/java/wang/suryai/a0002_AddTwoNumbers/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * @class Solution
+ * @package wang.suryai.a0002_AddTwoNumbers
+ * @date 2019/10/13 15:53
+ * Copyright (c) 2019 Endlexx
+ */
+package wang.suryai.a0002_AddTwoNumbers;
+
+/**
+ * @author Endlexx 2019/10/13 15:53
+ * @version V1.0
+ * modifyTime           author              description
+ * -------------------------------------------------------------------
+ */
+public class Solution {
+}
