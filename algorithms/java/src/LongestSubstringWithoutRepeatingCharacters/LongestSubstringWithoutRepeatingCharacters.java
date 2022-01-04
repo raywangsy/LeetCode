f97f41c71f6a608fbe79312ce655535c228ec856
Tsing Yun <imtsingyun@gmail.com>
2022-01-05 00:39:23 +0800

    Update LongestSubstringWithoutRepeatingCharacters.java

--- algorithms/java/src/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ algorithms/java/src/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -3,22 +3,21 @@ package LongestSubstringWithoutRepeatingCharacters;
 public class LongestSubstringWithoutRepeatingCharacters {
 
 	public int test(String s) {
-		int dp = 1;
-		int max = 1;
-		char[] chars = s.toCharArray();
-		StringBuilder str = new StringBuilder();
-
-		for (int i = 0; i < chars.length; i++) {
-			if (str.toString().indexOf(String.valueOf(chars[i])) > 0) {
-				str = new StringBuilder();
-				dp = 1;
-			} else {
-				dp += 1;
-				str = str.append(String.valueOf(chars[i]));
+		if (s == null || "".equals(s)) {
+			return 0;
+		}
+		int n = s.length();
+		int p = 0;
+		int i = 0;
+		int max = 0;
+		for (i = 0; i < n; i++) {
+			int idx = s.substring(p, i).indexOf(s.substring(i, i + 1));
+			if (idx >= 0) {
+				p = (p + 1) + idx;
 			}
-			max = Math.max(dp, max);
+			max = Math.max(max, (i + 1 - p));
 		}
 
-		return max;
+		return i - p;
 	}
 }
