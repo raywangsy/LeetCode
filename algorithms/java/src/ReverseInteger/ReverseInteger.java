f41021900e119a0512fac0e21ea5b8dc8e9f7b8b
Tsing Yun <imtsingyun@gmail.com>
2022-01-06 23:58:44 +0800

    Update ReverseInteger.java

--- algorithms/java/src/ReverseInteger/ReverseInteger.java
+++ algorithms/java/src/ReverseInteger/ReverseInteger.java
@@ -19,12 +19,29 @@ package ReverseInteger;
  * @date 2022/1/6 23:34
  */
 public class ReverseInteger {
-	public int reverse(int x) {
+
+	public int reverse1(int x) {
+		int res = 0;
+
+		while (x != 0) {
+			int prevRes = res;
+			res = res * 10 + x % 10;
+
+			if ((res - x % 10)/10 != prevRes) {
+				return 0;
+			}
+
+			x = x/10;
+		}
+		return res;
+	}
+
+	public int reverse2(int x) {
 		long res = 0;
 
 		while (x != 0) {
 			res = res * 10 + x % 10;
-			if (res > Integer.MAX_VALUE) {
+			if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
 				return 0;
 			}
 			x = x/10;
