30e9616eddd48fda0c067584689cbc49f50636fc
tsingyun <imtsingyun@gmail.com>
2022-01-19 14:12:47 +0800

    Create Solution.java

---
+++ algorithms/java/src/剑指Offer07重建二叉树/Solution.java
@@ -0,0 +1,21 @@
+package 剑指Offer07重建二叉树;
+
+
+public class Solution {
+	//Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		TreeNode root = new TreeNode(preorder[0]);
+
+		return null;
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
