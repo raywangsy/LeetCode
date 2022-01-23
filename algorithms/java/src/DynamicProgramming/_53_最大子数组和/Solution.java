e7c48e7ebfb5cf8445ff7b8a9c6f7e9256ea8074
Tsing Yun <imtsingyun@gmail.com>
2022-01-23 19:05:37 +0800

    最大子数组和

--- algorithms/java/src/DynamicProgramming/_53_最大子数组和/Solution.java
+++ algorithms/java/src/DynamicProgramming/_53_最大子数组和/Solution.java
@@ -1,4 +1,28 @@
 package DynamicProgramming._53_最大子数组和;
 
 public class Solution {
+
+	public static void main(String[] args) {
+
+	}
+	//输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+	//输出：6
+	//解释：连续子数组[4,-1,2,1] 的和最大，为6 。
+	public int maxSubArray(int[] nums) {
+		// dp[0] = -2
+		// dp[1] = 1
+		// dp[2] = dp[1] + (-3)
+		int[] dp = new int[nums.length];
+		dp[0] = nums[0];
+		int max = dp[0];
+		for (int i = 1; i < nums.length; i++) {
+			if (dp[i - 1] > 0) {
+				dp[i] = dp[i - 1] + nums[i];
+			} else {
+				dp[i] = nums[i];
+			}
+			max = Math.max(max, dp[i]);
+		}
+		return max;
+	}
 }
