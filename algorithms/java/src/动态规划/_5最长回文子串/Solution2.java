ac6dd4978699f33a3ee755b66cfc43a7d12605dc
Tsing Yun <imtsingyun@gmail.com>
2022-03-14 23:35:33 +0800

    Create Solution2.java

---
+++ algorithms/java/src/动态规划/_5最长回文子串/Solution2.java
@@ -0,0 +1,16 @@
+package 动态规划._5最长回文子串;
+
+public class Solution2 {
+	public static void main(String[] args) {
+
+	}
+
+	public String longestPalindrome(String s) {
+		if (s == null || s.length() == 0) {
+			return null;
+		}
+		int[] dp = new int[s.length()];
+
+		return null;
+	}
+}
