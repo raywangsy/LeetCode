c6b5a64825339351d05fb325e785eb010b43c3ab
Tsing Yun <imtsingyun@gmail.com>
2022-07-07 22:26:16 +0800

    Create FibNumberSolution01.java

---
+++ algorithms/java/src/com/leetcode/dp/dp001_509FibNumber/FibNumberSolution01.java
@@ -0,0 +1,21 @@
+package com.leetcode.dp.dp001_509FibNumber;
+
+/**
+ * 509. 斐波那契数 Fibonacci Number
+ * https://leetcode.cn/problems/fibonacci-number/
+ *
+ * @author Wang
+ * @version V1.0
+ * @date 2022/6/13 22:51
+ */
+public class FibNumberSolution01 {
+	public static void main(String[] args) {
+
+	}
+
+	public int fib(int n) {
+
+
+		return 0;
+	}
+}
