1ca050d5c8edc0b434ae2b7a2f39f0100302c774
Tsing Yun <56377217+imtsingyun@users.noreply.github.com>
2022-11-06 23:00:45 +0800

    summary

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ MD_HEAD = """## Leetcode 笔记
 本项目是基于 [@yihong0618](https://github.com/yihong0618) 的开源项目 [yihong0618/gitblog](https://github.com/yihong0618/gitblog) 创建而成 \n
 [🈯️ Easy](https://github.com/imtsingyun/LeetCode#easy)   [🈚️ Medium](https://github.com/imtsingyun/LeetCode#medium)   [🈲 Hard](https://github.com/imtsingyun/LeetCode#hard)
 \n
-### [常用解题方法总结](https://github.com/imtsingyun/LeetCode/blob/master/SUMMARY.md)
+### 常用解题方法总结：[Summary](https://github.com/imtsingyun/LeetCode/blob/master/SUMMARY.md)
 """
 
 BACKUP_DIR = "BACKUP"
